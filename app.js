f09e09436d252bebf81a5f9b5913c6687ee3747d
oynomak <oynomak@gmail.com>
2020-03-10 21:23:54 +0000

    Added different 4 functions with their conditions and URI

--- app.js
+++ app.js
@@ -33,6 +33,21 @@ var fs = require("fs");
 /* CRUD manipulation (READ, CREATE, GETbyID/UPDATE, DELETE) */
 http.createServer(function (request, response) {
 
+	if(request.url ==='/listUsers'){
+
+	}else if (request.url ==='/addUser') {
+
+	}else if (request.url ==='/:id') {
+		
+	}else if (request.url ==='/deleteUser') {
+		
+	}else if (request.url ==='/') {
+		
+	}else{
+		response.write("<h1>HTTP - 404</h1><p>Bad Request!</p>"); //write a error response
+    	response.end(); //end the response
+	}
+
 }).listen(port, function(){
  console.log(`Server running at http://${hostname}:${port}/`); //the server object listens on port 3000
 });
