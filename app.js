717445bd441b7c197eb346691d5df99a59b3c294
oynomak <oynomak@gmail.com>
2020-03-10 21:20:24 +0000

    Declared CRUD functionality to be able to manipulate RESTful NODE

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* ------ Simple HELLO WORLD example ------ */
-const http = require('http');
+
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -12,4 +13,26 @@ const server = http.createServer((request, response) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});*/
+
+/* ----------------------------------------- */
+
+
+/* ---------- Simple CRUD example ---------- */
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var fs = require("fs");
+
+/* Creates a new User to be added to the list of Users */
+
+
+/* CRUD manipulation (READ, CREATE, GETbyID/UPDATE, DELETE) */
+http.createServer(function (request, response) {
+
+}).listen(port, function(){
+ console.log(`Server running at http://${hostname}:${port}/`); //the server object listens on port 3000
 });
