de2520879780141f1bb3f0235358807733553e0f
oynomak <oynomak@gmail.com>
2020-03-11 07:33:47 +0000

    Separated methods for each fonctionality to call them inside conditions of CreateServer Method

--- app.js
+++ app.js
@@ -27,16 +27,6 @@ const port = 3000;
 
 var fs = require("fs");
 
-/* Creates a new User to be added to the list of Users */
-var user = {
-   "user4" : {
-      "name" : "mohit",
-      "password" : "password4",
-      "profession" : "teacher",
-      "id": 4
-   }
-}
-
 /* CRUD manipulation (READ, CREATE, GETbyID/UPDATE, DELETE) */
 http.createServer(function (request, response) {
 
@@ -47,30 +37,16 @@ http.createServer(function (request, response) {
 		    response.end( data );
     	});
 	}else if (request.url ==='/addUser') {
-		// First read existing users, then add new one
-   	    fs.readFile("users.json", 'utf8', function (err, data) {
-	        data = JSON.parse( data );
-	        data["user4"] = user["user4"];
-	        console.log( data );
-	        response.end( JSON.stringify(data));
-	    });
+
+	    addUser4(response);
+
 	}else if (request.url ==='/:id') {// Check if we can get this parameter here... I am not getting it... :(
-		// First read existing users, then get the specific one
-	    fs.readFile("users.json", 'utf8', function (err, data) {
-	        var users = JSON.parse( data );
-	        var user = users["user" + request.params.id];
-	        console.log( user );
-	        response.end( JSON.stringify(user));
-	    });
+
+		findUser(request.params.id, response);
+		
 	}else if (request.url ==='/deleteUser') {
-		// First read existing users, then delete the User2
-	    fs.readFile("users.json", 'utf8', function (err, data) {
-	        data = JSON.parse( data );
-	        delete data["user" + 2];
-	       
-	        console.log( data );
-	        response.end( JSON.stringify(data));
-	    });
+
+		deleteUser2(response);
 		
 	}else if (request.url ==='/') {
 		response.write('<h1>Hello World!<h1>'); //write a response
@@ -83,3 +59,52 @@ http.createServer(function (request, response) {
 }).listen(port, function(){
  console.log(`Server running at http://${hostname}:${port}/`); //the server object listens on port 3000
 });
+
+function addUser4(response){
+	/* Creates a new User to be added to the list of Users */
+	var user = {
+	   "user4" : {
+	      "name" : "mohit",
+	      "password" : "password4",
+	      "profession" : "teacher",
+	      "id": 4
+	   }
+	}
+
+	// Read through file that has users
+    fs.readFile("users.json", 'utf8', function (err, data) {
+
+		// First read existing users, then add new one
+	    data = JSON.parse( data );
+
+	    // Then add new User4
+	    data["user4"] = user["user4"];
+	    console.log( user["user4"] );
+	    response.end( JSON.stringify(data));
+	});
+}
+
+function findUser(userId, response){
+	// First read existing users, then get the specific one
+    fs.readFile("users.json", 'utf8', function (err, data) {
+        var users = JSON.parse( data );
+        var user = users["user" + userId];
+        console.log( user );
+        response.end( JSON.stringify(user));
+    });
+}
+
+function deleteUser2(response){
+	
+    fs.readFile("users.json", 'utf8', function (err, data) {
+    	// First read existing users
+        data = JSON.parse( data );
+
+        // Then delete the User2
+        delete data["user" + 2];
+       
+        console.log( data );
+        response.end( JSON.stringify(data));
+    });
+		
+}
